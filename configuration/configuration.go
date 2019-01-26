8536889c1f9cfc5ab3ca5dcbe434f736ce082cd0
yeric17 <yeric17@gmail.com>
2019-01-26 10:51:30 -0500

    se agrega la fucion de obtener la configuracion

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -1,5 +1,11 @@
 package configuration
 
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
 //Configuration es la estructura para config.json
 type Configuration struct {
 	Server   string
@@ -7,4 +13,21 @@ type Configuration struct {
 	User     string
 	Password string
 	Database string
+}
+
+//GetConfiguration es una funcion que retorna la configuracion
+func GetConfiguration() Configuration {
+	var c Configuration
+	file, err := os.Open("./config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	err = json.NewDecoder(file).Decode(&c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return c
 }
