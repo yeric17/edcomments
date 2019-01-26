7fa5ad99dc33619305677d3b8075466dccd43b93
yeric17 <yeric17@gmail.com>
2019-01-26 10:48:21 -0500

    se agrega la estructura para el config.json

---
+++ configuration/configuration.go
@@ -0,0 +1,10 @@
+package configuration
+
+//Configuration es la estructura para config.json
+type Configuration struct {
+	Server   string
+	Port     string
+	User     string
+	Password string
+	Database string
+}
