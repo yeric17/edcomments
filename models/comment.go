77ab67125435d4aac01e2d61d5340b05d4a923ab
yeric17 <yeric17@gmail.com>
2019-01-26 10:24:56 -0500

    se agrega el modelo comment

---
+++ models/comment.go
@@ -0,0 +1,15 @@
+package models
+
+import "github.com/jinzhu/gorm"
+
+//Comment es un modelo para la BD
+type Comment struct {
+	gorm.Model
+	UserID   uint      `json:"userId"`
+	ParentID uint      `json:"parentId"`
+	Votes    int32     `json:"votes"`
+	Content  string    `json:"content"`
+	HasVote  int8      `json:"hasVote" gorm:"-"`
+	User     []User    `json:"user,omitempty"`
+	Children []Comment `json:"children,omitempty"`
+}
