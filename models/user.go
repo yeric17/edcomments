453c3c3dc1da403d09a53516fb8db6c3b63e6b5f
yeric17 <yeric17@gmail.com>
2019-01-26 10:17:53 -0500

    se agrega la estructura usuario

---
+++ models/user.go
@@ -0,0 +1,15 @@
+package models
+
+import "github.com/jinzhu/gorm"
+
+//User es el modelo
+type User struct {
+	gorm.Model
+	Username        string    `json:"username" gorm:"not null;unique"`
+	Email           string    `json:"email" gorm:"not null;unique"`
+	Fullname        string    `json:"fullname" gorm:"not null"`
+	Password        string    `json:"password,omitempty" gorm:"not null;type:varchar(256)"`
+	ConfirmPassword string    `json:"confirmPassword,omitempty" gorm:"-"`
+	Picture         string    `json:"picture"`
+	Comments        []Comment `json:"comments,omitempty"`
+}
