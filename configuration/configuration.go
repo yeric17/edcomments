6fc5167ba409280027538bbed52a9c55bd0edddf
yeric17 <yeric17@gmail.com>
2019-01-26 11:02:45 -0500

    se agrega la funcion GetConnection para obtener la conexion a la base de datos

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -2,8 +2,13 @@ package configuration
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
+
+	//Es necesario el driver para que gorm pueda usar mysql
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 )
 
 //Configuration es la estructura para config.json
@@ -30,4 +35,22 @@ func GetConfiguration() Configuration {
 	}
 
 	return c
+}
+
+//GetConnection obtiene una conexion a la 	BD
+func GetConnection() *gorm.DB {
+	c := GetConfiguration()
+	//"user:password@tcp(server:port)/database?charset=utf8&parseTime=True&loc=Local"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		c.User,
+		c.Password,
+		c.Server,
+		c.Port,
+		c.Database,
+	)
+	db, err := gorm.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
 }
