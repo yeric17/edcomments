ebe671515646e708edce1c5461e257c1240416cb
yeric17 <yeric17@gmail.com>
2019-01-27 21:55:49 -0500

    se termina la funcion GetCommentAll

--- controllers/comment.go
+++ controllers/comment.go
@@ -3,7 +3,9 @@ package controllers
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/yeric17/edcomments/commons"
 	"github.com/yeric17/edcomments/configuration"
@@ -37,4 +39,84 @@ func CommentCreate(w http.ResponseWriter, r *http.Request) {
 	m.Code = http.StatusCreated
 	m.Message = "Comentario creado con éxito"
 	commons.DisplayMessage(w, m)
+}
+
+//CommentGetAll obtiene todos los comentarios
+func CommentGetAll(w http.ResponseWriter, r *http.Request) {
+	comments := []models.Comment{}
+	m := models.Message{}
+	user := models.User{}
+	vote := models.Vote{}
+
+	r.Context().Value(&user)
+	vars := r.URL.Query()
+
+	db := configuration.GetConnection()
+	defer db.Close()
+
+	cComment := db.Where("parent_id = 0")
+	if order, ok := vars["order"]; ok {
+		if order[0] == "votes" {
+			cComment = cComment.Order("votes desc, created_at DESC")
+		}
+	} else {
+		if idlimit, ok := vars["idlimit"]; ok {
+			registerByPage := 30
+			offset, err := strconv.Atoi(idlimit[0])
+			if err != nil {
+				log.Println("Error:", err)
+			}
+			cComment = cComment.Where("id BETWEEN ? AND ?", offset-registerByPage, offset)
+		}
+		cComment = cComment.Order("id DESC")
+	}
+	cComment.Find(&comments)
+
+	for i := range comments {
+		db.Model(&comments[i]).Related(&comments[i].User)
+		comments[i].User[0].Password = ""
+		comments[i].Children = commentGetChildren(comments[i].ID)
+		// Se busca el voto del usuario en sesión
+		vote.CommentID = comments[i].ID
+		vote.UserID = user.ID
+		count := db.Where(&vote).Find(&vote).RowsAffected
+		if count > 0 {
+			if vote.Value {
+				comments[i].HasVote = 1
+			} else {
+				comments[i].HasVote = -1
+			}
+		}
+	}
+
+	j, err := json.Marshal(comments)
+	if err != nil {
+		m.Code = http.StatusInternalServerError
+		m.Message = "Error al convertir los comentarios en json"
+		commons.DisplayMessage(w, m)
+		return
+	}
+
+	if len(comments) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(j)
+	} else {
+		m.Code = http.StatusNoContent
+		m.Message = "No se encontraron comentarios"
+		commons.DisplayMessage(w, m)
+	}
+
+}
+
+func commentGetChildren(id uint) (children []models.Comment) {
+	db := configuration.GetConnection()
+	defer db.Close()
+
+	db.Where("parent_id = ?", id).Find(&children)
+	for i := range children {
+		db.Model(&children[i]).Related(&children[i].User)
+		children[i].User[0].Password = ""
+	}
+	return
 }
